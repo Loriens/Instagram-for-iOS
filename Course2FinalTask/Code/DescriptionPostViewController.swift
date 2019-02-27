41c0b326a75520997deebdd0d451ae23638b7400
Loriens <lerd.95@mail.ru>
2019-02-27 22:33:25 +0300

    Add DescriptionPostViewController

---
+++ Course2FinalTask/Code/DescriptionPostViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DescriptionPostViewController.swift
+//  Course2FinalTask
+//
+//  Created by Vladislav on 27/02/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionPostViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
