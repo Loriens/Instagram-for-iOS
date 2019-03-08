35b15cc45bcb4432fd7f7ebbd4a6a9d332606d7f
Loriens <lerd.95@mail.ru>
2019-03-09 00:45:05 +0300

    Add Post and User structures

---
+++ Course2FinalTask/Code/Post.swift
@@ -0,0 +1,21 @@
+//
+//  Post.swift
+//  Course2FinalTask
+//
+//  Created by Vladislav on 09/03/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    var id: String
+    var authorID: String
+    var description: String
+    var image: String
+    var createdTime: Int
+    var currentUserLikesThisPost: Bool
+    var likedByCount: Int
+    var authorUsername: String
+    var authorAvatar: String
+}
---
+++ Course2FinalTask/Code/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Course2FinalTask
+//
+//  Created by Vladislav on 09/03/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var id: String
+    var username: String
+    var fullName: String
+    var avatar: String
+    var currentUserFollowsThisUser: Bool
+    var currentUserIsFollowedByThisIsUser: Bool
+    var followsCount: Int
+    var followedByCount: Int
+}
