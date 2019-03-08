9fa7a06957038823c95a525c5481ef2e2af6e37c
Loriens <lerd.95@mail.ru>
2019-03-08 12:27:54 +0300

    Add LoginViewController

---
+++ Course2FinalTask/Code/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Course2FinalTask
+//
+//  Created by Vladislav on 08/03/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
