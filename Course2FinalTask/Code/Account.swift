7d81b95db3059bd7139988233ddf726334a9d3dc
Loriens <lerd.95@mail.ru>
2019-03-08 12:50:55 +0300

    Add struct Account

---
+++ Course2FinalTask/Code/Account.swift
@@ -0,0 +1,14 @@
+//
+//  Account.swift
+//  Course2FinalTask
+//
+//  Created by Vladislav on 08/03/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import Foundation
+
+class Account: Codable {
+    var login: String
+    var password: String
+}
