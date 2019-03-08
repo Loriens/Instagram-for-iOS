544ce3fb384178c12b33655733a61fcce3cda854
Loriens <lerd.95@mail.ru>
2019-03-08 16:15:20 +0300

    Add class ServerQuery

---
+++ Course2FinalTask/Code/ServerQuery.swift
@@ -0,0 +1,20 @@
+//
+//  ServerQuery.swift
+//  Course2FinalTask
+//
+//  Created by Vladislav on 08/03/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Класс отвечает за запросы к серверу проекта.
+ Названия запросов соответствуют методам класса.
+ */
+
+class ServerQuery {
+    
+    
+    
+}
